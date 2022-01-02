afed3c464098656c150860ae0b4e2c7a89377a48
learn2410 <learn2410@mail.ru>
2022-01-02 11:58:34 +0500

    step 11 (rendering wines from  wine.xlsx)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
+import pandas
 
 def get_firm_age():
     age=str(datetime.now().year - 1920)
@@ -14,7 +15,8 @@ env = Environment(
 
 template = env.get_template('template.html')
 rendered_page = template.render(
-    firm_age = get_firm_age()
+    firm_age=get_firm_age(),
+    wines=pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='records'),
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
