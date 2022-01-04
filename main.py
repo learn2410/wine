af3abf0de6b1dfe5ffaaa18fcc9115a3bad097e4
learn2410 <learn2410@mail.ru>
2022-01-04 19:20:28 +0500

    step 14 - wines with category

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 import pandas
+import collections
+
 
 def get_firm_age():
-    age=str(datetime.now().year - 1920)
-    years='год' if age[-1] == '1' else 'года' if '2' <= age[-1] <= '4' else 'лет'
+    age = str(datetime.now().year - 1920)
+    years = 'год' if age[-1] == '1' else 'года' if '2' <= age[-1] <= '4' else 'лет'
     return f'{age} {years}'
+
+
+def get_group_wines(xlsx):
+    df = pandas.read_excel(xlsx, sheet_name='Лист1').fillna('').to_dict(orient='records')
+    wines = collections.defaultdict(list)
+    grouplist = list(set(x['Категория'] for x in df))
+    # move wines to top of list (the wine can be pink, sparkling e.t.c)
+    grouplist.sort(key=lambda k: ' ' + k if ' вина' in k else k)
+    for category in grouplist:
+        wines[category] = []
+    for w in df:
+        wines[w['Категория']] += [w]
+    return wines
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -16,7 +32,7 @@ env = Environment(
 template = env.get_template('template.html')
 rendered_page = template.render(
     firm_age=get_firm_age(),
-    wines=pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='records'),
+    wines=get_group_wines('wine2.xlsx'),
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
