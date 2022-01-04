c309ef3da34a5b4682c5715232c976b852a2085d
learn2410 <learn2410@mail.ru>
2022-01-04 09:27:28 +0500

    step 12 - pprint and grouping of wines

---
+++ step12_grouping_wines.py
@@ -0,0 +1,6 @@
+import pandas
+from pprint import pprint
+
+g = pandas.read_excel('wine2.xlsx', sheet_name='Лист1').fillna('').groupby('Категория')
+wines={n:g.get_group(n).to_dict(orient='records') for n in g.groups.keys()}
+pprint(wines)
